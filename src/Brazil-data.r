a5d1435176a3b79921ea5aecf3ad4fe1144de0d7
Elizaveta Semenova <elizaveta.p.semenova@gmail.com>
2022-04-13 12:21:25 +0100

    preprocess population data

--- src/Brazil-data.r
+++ src/Brazil-data.r
@@ -1,20 +1,19 @@
-# Load required libraries
-library(data.table); library(ggplot2); library(ggsci); library(viridis); library(gtools)
-library(MGLM); library(fitdistrplus); library(actuar); library(posterior); library(bayesplot)
-library(openxlsx); library(lubridate); library(tidyr); library(repr); library(ggpubr)
-library(readr); library(tidyverse); library(survival); library(survminer); library(here)
-library(mgcv); library(visreg); library(locfit)
-
+library(tidyverse) 
 library(here)
+library(lubridate)
+
+#----------------------------------------
+# Brazil hospitalization/death data
+#----------------------------------------
 
 ## You will need to download the original data and put it in the data folder in covid19pediatric: 
 ## https://opendatasus.saude.gov.br/dataset/srag-2021-e-2022 (2021/22)
 ## https://opendatasus.saude.gov.br/dataset/srag-2020 (2020)
 ## scroll down to the CSV link and click Explorar -> Baixar
 ## Update the dates below depending on the vintage you download:
-sivep_2020 <- read_csv2(here("data/INFLUD20-21-03-2022.csv"))
-sivep_2021 <- read_csv2(here("data/INFLUD21-21-03-2022.csv"))
-sivep_2022 <- read_csv2(here("data/INFLUD22-21-03-2022.csv"))
+sivep_2020 <- read_csv2(here("data/raw_data/INFLUD20-21-03-2022.csv"))
+sivep_2021 <- read_csv2(here("data/raw_data/INFLUD21-21-03-2022.csv"))
+sivep_2022 <- read_csv2(here("data/raw_data/INFLUD22-21-03-2022.csv"))
 cols_of_interest <- c("SG_UF", "SG_UF_NOT", "ID_MUNICIP", "EVOLUCAO", "DT_SIN_PRI", "DT_EVOLUCA", "HOSPITAL",
                      "DT_INTERNA", "NU_IDADE_N", "CS_SEXO", "CLASSI_FIN", "DT_NASC",
                      "UTI", "DT_ENTUTI", "DT_SAIDUTI", "SUPORT_VEN", "SATURACAO")
@@ -22,7 +21,7 @@ sivep_2020 <- sivep_2020[, cols_of_interest]
 sivep_2021 <- sivep_2021[, cols_of_interest]
 sivep_2022 <- sivep_2022[, cols_of_interest]
 df_SIVEP_original <- rbind(sivep_2020, sivep_2021, sivep_2022)
-rm(sivep_2020); rm(sivep_2021); rm(sivep_2022)
+rm(sivep_2020); rm(sivep_2021); rm(sivep_2022); rm(cols_of_interest)
 
 # # get only suspected and confirmed deaths
 df_SIVEP <- df_SIVEP_original[which(df_SIVEP_original$CLASSI_FIN %in% c(4,5)),] #4 is suspected, 5 is confirmed
@@ -101,4 +100,48 @@ sum(is.na(df_SIVEP_proc$DateOutcome)) # 30000 missing date of outcome
 sum(df_SIVEP_proc$Outcome == "Unknown_Or_Missing") # 26000 missing outcome
 sum(is.na(df_SIVEP_proc$time)) # 30000 missing either date of symptoms or date of outcome
 
+rm(list=setdiff(ls(), "df_SIVEP_proc"))
+
 saveRDS(df_SIVEP_proc, here("data/df_SIVEP.rds"))
+
+#----------------------------------------
+# Brazil - population Age distribution
+#----------------------------------------
+
+pop_bra_func <- function() {
+  library(readxl)
+  pop_data <- read_excel(here("data/raw_data/BRA_projecoes_2018_populacao_idade_simples_2010_2060_20201209.xls"),sheet=1,skip=195) 
+  
+  pop_data <- pop_data %>%
+    select(IDADE,  "2020") %>%
+    slice(-c(1, 93:95))
+  
+  pop_df <-  pop_data %>% 
+    rename(Age = IDADE, pop_age = "2020")  %>%
+    mutate(Age = ifelse(Age=="90+", 90, Age),
+           Age = as.numeric(Age)) %>%
+    arrange(Age)
+  
+  pop_df <- data.frame(pop_df)
+  
+  # assume even distributions among 90+
+  pop_90 <- pop_df$pop_age[91] / 10
+  append_90 <- data.frame(Age = c(90:99), pop_age = rep(pop_90, 10))
+  
+  pop_df <- pop_df %>% slice(-nrow(pop_df))
+  pop_df <- rbind(pop_df, append_90)
+  
+  if (sum(pop_df$pop_age) == sum(pop_data["2020"])) {
+    print("the totals are correct")
+  } else {
+    print("the totals are wrong")
+  }
+  
+  return(pop_df)
+}
+
+bra_pop <- pop_bra_func()
+
+saveRDS(bra_pop, here("data/bra_pop.rds"))
+
+
